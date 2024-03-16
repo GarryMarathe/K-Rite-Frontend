b1b012ed310e3b307177fc29d56a05520dfe474e
GauravMarathe <gauravmarathe6468@gmail.com>
2024-03-16 17:47:22 +0530

    search

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 // Function For toggle dropdown visibility
 
-let dropDown = function (i) {
+let droplist = function (i) {
     if (i.style.display === "none") {
       i.style.display = "block";
     } else {    
@@ -8,39 +8,39 @@ let dropDown = function (i) {
     }
   };
   
-  let pressedBtn = document.getElementById("headBtn");
-  let listElements = document.getElementById("btnElement");
+  let pressedBtn = document.getElementById("dropDown");
+  let listElements = document.getElementById("dropElements");
   
-  let pressedBtn1 = document.getElementById("headBtn1");
-  let listElements1 = document.getElementById("btnElement1");
+  let pressedBtn1 = document.getElementById("dropDown1");
+  let listElements1 = document.getElementById("dropElements1");
   
-  let pressedBtn2 = document.getElementById("headBtn2");
-  let listElements2 = document.getElementById("btnElement2");
+  let pressedBtn2 = document.getElementById("dropDown2");
+  let listElements2 = document.getElementById("dropElements2");
   
-  let pressedBtn3 = document.getElementById("headBtn3");
-  let listElements3 = document.getElementById("btnElement3");
+  let pressedBtn3 = document.getElementById("dropDown3");
+  let listElements3 = document.getElementById("dropElements3");
   
-  let pressedBtn4 = document.getElementById("headBtn4");
-  let listElements4 = document.getElementById("btnElement4");
+  let pressedBtn4 = document.getElementById("dropDown4");
+  let listElements4 = document.getElementById("dropElements4");
   
   pressedBtn.addEventListener("click", function () {
-    dropDown(listElements);
+    droplist(listElements);
   });
   
   pressedBtn1.addEventListener("click", function () {
-    dropDown(listElements1);
+    droplist(listElements1);
   });
   
   pressedBtn2.addEventListener("click", function () {
-    dropDown(listElements2);
+    droplist(listElements2);
   });
   
   pressedBtn3.addEventListener("click", function () {
-    dropDown(listElements3);
+    droplist(listElements3);
   });
   
   pressedBtn4.addEventListener("click", function () {
-    dropDown(listElements4);
+    droplist(listElements4);
   });
 
 // Function to toggle checkbox state
@@ -63,29 +63,26 @@ document.getElementById('selectAllBrands').addEventListener('click', function() 
   }, this); // Pass 'this' as the second argument to ensure it refers to the checkbox element
 });
 
+document.getElementById("searchForm").addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevent the default form submission behavior
+
+  let searchTerm = document.getElementById("searchInput").value.toLowerCase();
+  let rows = document.querySelectorAll(".tableData tbody tr");
+
+  // Loop through each row and hide/show based on search term
+  rows.forEach(function(row) {
+    let companyName = row.querySelector(".brand-Name").textContent.toLowerCase();
+    if (companyName.includes(searchTerm)) {
+      row.style.display = "table-row";
+    } else {
+      row.style.display = "none";
+    }
+  });
+});
 
 
-  document
-    .getElementById("searchForm")
-    .addEventListener("submit", function (event) {
-      event.preventDefault();
-  
-      let searchTerm = document.getElementById("searchInput").value.toLowerCase();
-  
-      let rows = document.querySelectorAll(".tableData tbody tr");
-  
-      // Loop through each row and hide/show based on search term
-      rows.forEach(function (row) {
-        let companyName = row
-          .querySelector(".brand-Name")
-          .textContent.toLowerCase();
-        if (companyName.includes(searchTerm)) {
-          row.style.display = "table-row";
-        } else {
-          row.style.display = "none";
-        }
-      });
-    });
+
+
   
   // SideBar
   
